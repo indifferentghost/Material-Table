a5c000296ae4a2af70a162f1f4fec7cf00d53863
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-01-11 18:16:10 -0500

    init

---
+++ config/webpack.base.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const BASE_DIR = path.resolve(__dirname, '../');
+const PUBLIC_DIR = path.resolve(BASE_DIR, './public');
+
+module.exports = {
+  entry: path.resolve(BASE_DIR, './src/index.tsx'),
+  output: {
+    path: path.resolve(BASE_DIR, './dist'),
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js(x?)$/,
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(PUBLIC_DIR, './index.ejs'),
+      filename: 'index.html',
+      title: 'music-app',
+      publicDir: PUBLIC_DIR,
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.wasm', '.mjs', '.js', '.json'],
+  },
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
+const merge = require('webpack-merge');
+
+const baseConfig = require('./webpack.base.js');
+
+module.exports = merge(baseConfig, {
+  plugins: [
+    new HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin()
+  ],
+  stats: 'minimal',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    hot: true,
+    // redirect 404s to /index.html
+    historyApiFallback: true,
+  },
+  devtool: 'eval-source-map',
+});
